97d6d718e183cb12cbde3a7778447114795c7033
PAVANI   BODDU <100023711+Pavani7777777@users.noreply.github.com>
2023-05-29 14:26:40 +0530

    def fun(n):     c=0     for i in n:         if i in 'aeiouAEIOU':             c=c+1     return c n=input().split() l=[] for i in n:     l.append(fun(i)) s=max(l) print(l.count(s))

---
+++ count_words_with_maximum_vowels.py
@@ -0,0 +1,13 @@
+def fun(n):
+    c=0
+    for i in n:
+        if i in 'aeiouAEIOU':
+            c=c+1
+    return c
+n=input().split()
+l=[]
+for i in n:
+    l.append(fun(i))
+s=max(l)
+print(l.count(s))
+        
