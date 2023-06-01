fd9fe088fac0376eb8c54a9cd6b4e0c41674d3d3
PAVANI   BODDU <100023711+Pavani7777777@users.noreply.github.com>
2023-06-01 11:54:53 +0530

    s=input() c=0 for i in s:     if i=='A' or i=='B' or i=='C' or i=='D' or i=='E' or i=='F' or i=='G' or i=='H' or i=='I' or i=='J' or i=='K' or i=='L' or i=='M' or i=='N' or i=='O' or i=='P' or i=='Q' or i=='R' or i=='S' or i=='T' or i=='U' or i=='V' or i=='W' or i=='X' or i=='Y' or i=='Z':         c=c+1 print(c)

---
+++ uppercase_letters_in_a_string.py
@@ -0,0 +1,6 @@
+s=input()
+c=0
+for i in s:
+    if i=='A' or i=='B' or i=='C' or i=='D' or i=='E' or i=='F' or i=='G' or i=='H' or i=='I' or i=='J' or i=='K' or i=='L' or i=='M' or i=='N' or i=='O' or i=='P' or i=='Q' or i=='R' or i=='S' or i=='T' or i=='U' or i=='V' or i=='W' or i=='X' or i=='Y' or i=='Z':
+        c=c+1
+print(c)
